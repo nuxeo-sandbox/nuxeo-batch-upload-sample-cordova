971f667b1ec88f1d7ca6175fd1278e67b83e4b18
Michael GENA <mgena@nuxeo.com>
2016-05-04 14:45:57 +0200

    Adjustments made on the upload

--- www/js/index.js
+++ www/js/index.js
@@ -11,20 +11,20 @@ function processInputFile() {
 
     window.resolveLocalFileSystemURL(url, function (fileEntry) {
             fileEntry.file(function (file) {
-                // Overriding localURL attribute with absolute file path
-                file.localURL = fileEntry.toURL();
-
-                // Overriding type attribute as it is otherwise empty
-                file.type = 'image/png';
-
-                uploadFileToNuxeoPlatform(file)
+              file.localURL = fileEntry.toURL();
+              var reader = new FileReader();
+            	reader.onloadend = function (evt) {
+            		var blob = new Blob([evt.target.result]);
+            		var nuxeoBlob = new Nuxeo.Blob({content: blob, name: file.name, mimeType: 'image/png'});
+            		uploadFileToNuxeoPlatform(nuxeoBlob);
+            	};
+              reader.readAsArrayBuffer(file);
             })
         }
     );
 }
 
-
-function uploadFileToNuxeoPlatform(file) {
+function uploadFileToNuxeoPlatform(blob) {
     var nuxeo = new Nuxeo({
         baseURL: 'http://engie.cloud.nuxeo.com/nuxeo/',
         auth: {
@@ -33,18 +33,28 @@ function uploadFileToNuxeoPlatform(file) {
             password: 'Administrator'
         }
     });
-
-    var blob = new Nuxeo.Blob({content: file});
-
     nuxeo
         .batchUpload()
         .upload(blob)
         .then(function (response) {
-            console.info('Response received.');
-            var batchBlob = response.blob;
-            if (batchBlob instanceof Nuxeo.BatchBlob) {
-                console.info('BatchBlob is: ' + JSON.stringify(batchBlob));
-            }
+          return nuxeo.operation('Document.Create')
+         .params({
+           type: 'Picture',
+           name: 'My image',
+           properties: 'dc:title=My image \ndc:description=Image uploaded via cordova-plugin-file.'
+         })
+         .input('/default-domain/workspaces/Installations')
+         .execute()
+         .then(function(doc) {
+           console.info("Empty document created: "+JSON.stringify(doc));
+           console.info("Url to the document: http://engie.cloud.nuxeo.com/nuxeo/nxdoc/default/"+doc.uid+"/view_documents");
+           return nuxeo.operation('Blob.AttachOnDocument')
+             .param('document', doc.path)
+             .input(response.blob)
+             .execute({ schemas: ['dublincore', 'file']});
+         })
+       }).then(function(res){
+          console.info("File uploaded and attached to document");
         })
         .catch(function (error) {
             console.error("An error occurred uploading blob: " + error.message);
